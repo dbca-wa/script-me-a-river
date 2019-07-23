cb85ce2c6c72de012d1f4580328cff8ae33f620d
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2019-07-23 16:52:54 +0800

    Init

---
+++ install.R
@@ -0,0 +1,5 @@
+install.packages("devtools")
+install.packages("here")
+
+remotes::install_github("ropensci/ckanr")
+remotes::install_github("Bartesto/monitoR")  # TODO migrate and update
---
+++ plots/README.md
@@ -0,0 +1,2 @@
+# Contents
+This folder contains surfer plots outside of version control.
