f234d3a02dd941c256501980b730579c25d615ba
Bart Huntley <barthuntley@iinet.net.au>
2019-08-02 15:31:00 +0800

    surfR specific functions

--- setup.R
+++ setup.R
@@ -4,6 +4,7 @@ library(here)
 library(ckanr)
 library(monitoR)
 
+
 #------------------------------------------------------------------------------#
 # Settings
 if (file.exists("~/.Rprofile")) source("~/.Rprofile")
@@ -63,3 +64,57 @@ upload_to_ckan <- function(data,
   }
   r$id
 }
+
+#' Create a CKAN resource from surfer plot/s.
+#'
+#'
+#' @details The surfer plots residing in the Project directory `plots/` will 
+#'   be uploaded to CKAN as a resource with the file name as a title. It can 
+#'   handle multiple plots. 
+#' @examples
+#' \notrun{
+#' CKAN_surfR_upload()
+#' }
+
+CKAN_surfR_upload <- function(){
+  plts <- list.files(path = here::here("plots"), pattern = ".pdf")
+  for(plt in plts){
+    resource_title <- stringr::str_split(plt, pattern = "\\.")[[1]][1]
+    dataset_id <- "c1be7f9c-4ac8-4ed4-a370-5173a7fedb71"
+    f <- stringr::str_split(plt, pattern = "\\.")[[1]][2]
+    resource_filename <- here::here("plots", plt)
+    resource_id=NULL
+    
+    
+    cat("No resource ID given, creating a new resource for",
+        resource_title, ": ")
+    r <- ckanr::resource_create(package_id = dataset_id,
+                                format = f,
+                                name = resource_title,
+                                upload = resource_filename)
+    r$id
+    cat("\n")
+  }
+}
+
+#' Helper function to remove one or more surfer plots from CKAN
+#' 
+#' 
+#' @details Blunt function to remove ALL surfer plots from the
+#'   water-quality-monitoring-swan-canning-estuary-2019 dataset. Intended
+#'   as a quick clean up tool for use while testing.
+#' @param result is the result from running a the following resource query
+#'   result <- ckanr::resource_search(q = "name:surfer")
+#' @examples
+#' \notrun{
+#' surfR_remove(result = ckanr::resource_search(q = "name:surfer"))
+#' } 
+CKAN_surfR_remove <- function(result){
+  for(i in seq_along(result)){
+    cat("Deleting", result$results[[i]]$id, ": ")
+    ckanr::resource_delete(id = result$results[[i]]$id)
+    cat("\n")
+  }
+}
+
+
